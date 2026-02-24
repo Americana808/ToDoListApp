1c499e24c2570ecb5ee5d42fdc4972e57ba50696
Marco <mportillo1605@gmail.com>
2026-02-23 23:49:58 -0700

    added

--- ToDoListApp/Controllers/TasksController.cs
+++ ToDoListApp/Controllers/TasksController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace ToDoListApp.Controllers
 {
+
+
     [Authorize]
     [ApiController]
     [Route("tasks")]
